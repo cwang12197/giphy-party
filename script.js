acb4f36bc3c28e297cae23967c823c863ae236be
Christina Wang <christinawang@fb.com>
2022-06-16 19:19:04 -0700

    finished

--- script.js
+++ script.js
@@ -2,39 +2,60 @@ const API_KEY = "KdV88g3E15so7dWCPOAbJQ8bnSGXRo4E&q=";
 const API_FRONT = "http://api.giphy.com/v1/gifs/search?api_key=";
 const API_URL = API_FRONT + API_KEY ;
 const limit = 9;
-const pages = 1;
-const offset = pages * limit;
 const rating = "g";
 
 const searchBarEl = document.querySelector("#search-bar");
 const submitButton = document.querySelector("#submit-button");
 const gifContainer = document.querySelector("#gif-container"); //clear this after search
 const searchInput = document.querySelector("#search");
+const loadMoreGifs = document.querySelector("#load-more");
 
+var pages = 0;
+var offset = pages * limit;
 
-async function getResults() {
-    console.log(searchInput.value);
-    const url = `${API_URL}${searchInput.value}${rating}`
+let searchTerm = "";
+
+async function getResults(searchTerm, offset) {
+    console.log(searchTerm);
+    const url = `${API_URL}${searchTerm}&limit=${limit}&rating=${rating}&offset=${offset}`;
     const response = await fetch(url); //fetch the url with the search
     const results = await response.json();
     const resultData = results.data;
-    for (let i = 0; i < offset; i++) 
     resultData.forEach(result => {
     displayResults(result);
    })
 }
 
 function displayResults(data) {
-    console.log(data.images.original.url);
+    //console.log(data.images.original.url);
         gifContainer.innerHTML += `
-        <img id = "gifs" src = ${data.images.original.url} alt = "gif">
+        <img id = "" src = ${data.images.original.url} alt = "${data.title}">
         `
 }
 
 searchBarEl.addEventListener('submit', event => {
     event.preventDefault();
-    getResults();
+   // remove hidden element
+   loadMoreGifs.classList.remove("hidden");
+   
+   //clear gifResults
+   gifContainer.innerHTML = "";
+    pages = 0;
+    searchTerm = searchInput.value;
+    console.log(searchTerm);
+   getResults(searchTerm, (pages++)*limit);
+   //searchInput.value = " "; //reset search
+
 });
+
+loadMoreGifs.addEventListener('click', event => {
+    offset = (pages++)*limit;
+    console.log("after loading new pages: ", searchTerm);
+    getResults(searchTerm, offset);
+    searchInput.value = " ";
+});
+
+
 
 
 
